50331f07f5c4534ea9853a2d22c8487d43427690
Ricardo Sousa <sousa.ricardo10@gmail.com>
2013-11-11 17:45:36 +0000

    Initial commit.

---
+++ workspace/HelloWorld/src/Application/Employee.java
@@ -0,0 +1,94 @@
+package Application;
+
+/**
+ * Class for represent one Employee.
+ * */
+public class Employee
+{
+	private int    empNum;
+	private String name;
+	private String city;
+	
+	/**
+	 * Creates an instance of {@link Employee}, without arguments.
+	 * */
+	public Employee()
+	{
+		this.city = "Lisboa";
+	}
+	
+	/**
+	 * Creates an instance of {@link Employee}, with arguments.
+	 * 
+	 * @param empNum The Number of the {@link Employee}
+	 * @param name   The name of the {@link Employee}
+	 * */
+	public Employee(int empNum, String name)
+	{
+		// Already have the information of the city, This calls the default .ctor.
+		this();
+		
+		this.empNum = empNum;
+		this.name   = name;
+	}
+	
+	/**
+	 * Creates an instance of {@link Employee}, with arguments.
+	 * 
+	 * @param empNum The Number of the {@link Employee}
+	 * @param name   The name of the {@link Employee}
+	 * @param city 	 The city of the {@link Employee}
+	 * */
+	public Employee(int empNum, String name, String city)
+	{
+		this.empNum = empNum;
+		this.name   = name;
+		this.city   = city;
+	}
+	
+	/*
+	 * Getters/Setters methods.
+	 * */
+	public int getEmpNum() 
+	{
+		return empNum;
+	}
+	
+	public void setEmpNum(int empNum) 
+	{
+		this.empNum = empNum;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	// setName Overload 
+	public void setName(String firstName, String lastName)
+	{
+		this.name = firstName + " " + lastName;
+	}
+	
+	public String getCity() 
+	{
+		return city;
+	}
+	
+	public void setCity(String city) 
+	{
+		this.city = city;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "Employee [empName=" + this.getName() + " empNum=" + this.getEmpNum() + ""
+				+ " empCity=" + this.getCity() +"]";
+	}
+}
--- workspace/HelloWorld/src/Application/HelloWorld.java
+++ workspace/HelloWorld/src/Application/HelloWorld.java
@@ -18,6 +18,7 @@ public class HelloWorld
 		//String empty = new String("");
 		//String noString = null;
 		
+		/*
 		System.out.println("Foram passados " + args.length + " parametros.");
 		  
         
@@ -26,5 +27,26 @@ public class HelloWorld
 		str = (args.length > 0)? str + " " + args[0] : str;
 		
 		System.out.println(str);
+		*/
+		
+		/*
+		// Criar objecto do tipo empregado utilizando o constructor por omissão.
+		Employee emp = new Employee();
+		// Atribuir valores aos campos do empregado, utilizando os setters methods.
+		emp.setEmpNum(1234);
+		emp.setName("Ricardo Sousa");
+		emp.setCity("Lisboa");
+		*/
+		
+		// Criar objecto do tipo empregado utilizando o contructor com parâmetros.
+		Employee emp = new Employee(1234, "Ricardo Sousa", "Lisboa");
+		
+		// String name = emp.getName();
+		// System.out.println("Hello " + name + ", from " + emp.getCity());
+		
+		System.out.println("Hello " + emp.toString());// Não é necessário o .toString()
+		
+		Object emp2 = new Employee(1234, "Ricardo Sousa", "Lisboa");
+		System.out.println("Hello " + emp2.toString());
 	}
 }
