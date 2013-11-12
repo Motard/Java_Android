dd6c3fa7b2f7c54c63fe5f25388a3bda8ee4ae03
Ricardo Sousa <sousa.ricardo10@gmail.com>
2013-11-12 01:36:06 +0000

    Add Other HelloWorld project for test the joda-time library.

--- workspace/HelloWorld/src/Application/HelloWorld.java
+++ workspace/HelloWorld/src/Application/HelloWorld.java
@@ -6,7 +6,7 @@ package Application;
  * */
 public class HelloWorld 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 		/*
 		if (args.length != 0)
